98f08d683d6ff538cb1bedde8dc4213fe350f04e
Samuel Ayvazyan <samuel.ayvazyan.fd@gmail.com>
2018-01-18 00:36:28 +0400

    Add posts service.

---
+++ src/app/service/posts/posts.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+export interface Post {
+  title: "",
+  url: ""
+}
+
+@Injectable()
+export class PostsService {
+
+  constructor(private afs: AngularFirestore) { }
+
+  getPosts() {
+    return this.afs.collection("posts").snapshotChanges()
+      .map( post => {
+        return post.map ( a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })
+      });
+  }
+  addPost(post: Post) {
+    this.afs.collection("posts").add(post);
+  }
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
+  updatePost(postId, post: Post) {
+    this.afs.doc('posts/'+postId).update(post);
+  }
+
+}
