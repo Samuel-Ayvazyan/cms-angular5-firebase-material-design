3dbb831fd290af60c886f0ac9efdec10410d354d
Samuel Ayvazyan <samuel.ayvazyan.fd@gmail.com>
2018-01-18 00:28:48 +0400

    Add menus service.

---
+++ src/app/service/menus/menus.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+export interface Menu {
+  title: "",
+  url: ""
+}
+
+@Injectable()
+export class MenusService {
+
+  constructor(private afs: AngularFirestore) { }
+
+  getMenus() {
+    return this.afs.collection("menus").snapshotChanges()
+      .map( menu => {
+        return menu.map ( a => {
+          const data = a.payload.doc.data() as Menu;
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })
+      });
+  }
+  addMenu(menu: Menu) {
+    this.afs.collection("menus").add(menu);
+  }
+  deleteMenu(menuId) {
+    this.afs.doc('menus/'+menuId).delete();
+  }
+  updateMenu(menuId, menu: Menu) {
+    this.afs.doc('menus/'+menuId).update(menu);
+  }
+
+}
